66f94fddcb55b58924becf9c23725acf26e3850b
Arif Laksonodhewo <arifldhewo234@gmail.com>
2023-10-20 16:32:21 +0700

    Adding function profile page

--- src/main/java/org/example/pageObject/profile/Profile.java
+++ src/main/java/org/example/pageObject/profile/Profile.java
@@ -23,10 +23,10 @@ public class Profile {
     //Following & Follower Object
 
     @FindBy(xpath = "//p[.='0 Followers']")
-    private WebElement buttonFollower;
+    private WebElement tabFollower;
 
     @FindBy(xpath = "//p[.='1 Following']")
-    private WebElement buttonFollowing;
+    private WebElement tabFollowing;
 
     //General Object
 
@@ -56,11 +56,77 @@ public class Profile {
     @FindBy(xpath = "//div[@class='flex justify-between items-center']/button[1]")
     private WebElement buttonAddToCart;
 
-    //Another Profile
+    //Another Profile Object
 
     @FindBy(xpath = "//button[@class=' btn-primary place-self-end mb-24 w-fit px-7 py-1 rounded-full']")
     private WebElement buttonFollow;
 
     @FindBy(xpath = "//h1[@class='font-bold col-span-2 text-xl md:text-xl flex']")
     private WebElement anotherUsername;
+
+    //Function
+
+    public String getTitleAlert() {
+        return SweetAlertTitle.getText();
+    }
+
+    public void clickButtonBackToHome() {
+        buttonBackToHome.click();
+    }
+
+    //Following & Follower Function
+
+    public void clickTabFollower() {
+        tabFollower.click();
+    }
+
+    public void clickTabFollowing() {
+        tabFollowing.click();
+    }
+
+    //General Function
+
+    public void clickButtonComment() {
+        buttonComment.click();
+    }
+
+    public void clickButtonLike() {
+        buttonLike.click();
+    }
+
+    public void clickButtonMore() {
+        buttonMore.click();
+    }
+
+    public void clickButtonDelete() {
+        buttonDelete.click();
+    }
+
+    public void clickButtonRecook() {
+        buttonRecook.click();
+    }
+
+    public String getTextPostCategory(){
+        return textPostCategory.getText();
+    }
+
+    //Recipe Function
+
+    public void clickOpenRecipeElement() {
+        openRecipeElement.click();
+    }
+
+    public void clickButtonAddToCart () {
+        buttonAddToCart.click();
+    }
+
+    //Another Profile Function
+
+    public void clickButtonFollow() {
+        buttonFollow.click();
+    }
+
+    public String getAnotherUsername() {
+        return anotherUsername.getText();
+    }
 }
