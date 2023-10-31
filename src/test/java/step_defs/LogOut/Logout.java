81d86ec7bdd15a8cba7bb1880abff09f6bbd44ba
ilhm0 <muchsininilham@gmail.com>
2023-10-31 21:57:39 +0700

    adding log out feature

---
+++ src/test/java/step_defs/LogOut/Logout.java
@@ -0,0 +1,72 @@
+package step_defs.LogOut;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.pageObject.logout.LogoutButton;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import step_defs.Hooks;
+
+public class Logout {
+    private final WebDriver driver = Hooks.driver;
+
+    LogoutButton logoutButton = new LogoutButton(driver);
+
+    @Given("User already on Cookit page")
+    public void userAlreadyOnCookitLandingPage() throws InterruptedException{
+        Thread.sleep(3000);
+        Assert.assertTrue(logoutButton.verifyLandingPageTitle());
+    }
+
+    @When("User click on Sign In button")
+    public void userClickSignInButton() {
+        logoutButton.clickButtonLanding();
+    }
+
+    @And("User input {string} as username and {string} as password")
+    public void userInputUsername(String username, String password) {
+        logoutButton.setInputUsername(username);
+        logoutButton.setInputPassword(password);
+    }
+
+
+    @And("User click login button")
+    public void clickLogInButton() {
+        logoutButton.clickButtonLogin();
+    }
+
+
+
+    @And("User click on profile menu")
+    public void userClickOnProfileMenu() throws InterruptedException{
+        Thread.sleep(5000);
+        logoutButton.clickProfileMenu();
+    }
+
+    @When("User click logout")
+    public void userClickLogout() {
+        logoutButton.clikLogoutButton();
+    }
+
+
+    @And("Verify logout pop up is displayed")
+    public void verifyLoginTextIsDisplayed() {
+        Assert.assertTrue(logoutButton.verifyTextOutDisplayed());
+    }
+
+
+    @And("User click yes")
+    public void userClickYes() {
+        logoutButton.clickYesButton();
+    }
+
+
+    @Then("user back to login page")
+    public void userBackToLoginPage() throws InterruptedException{
+        Thread.sleep(4000);
+        Assert.assertTrue(logoutButton.verifyLoginPage());
+    }
+}
---
+++ src/test/resources/features/13.LogOut.feature
@@ -0,0 +1,15 @@
+@LogOut
+Feature: Log Out
+  As a user i want to Log out may Account
+
+  @positive
+  Scenario: User want to log out account
+    Given User already on Cookit page
+    When User click on Sign In button
+    And User input "badaklaut" as username and "Badaklaut1" as password
+    And User click login button
+    And User click on profile menu
+    When User click logout
+    And Verify logout pop up is displayed
+    And User click yes
+    Then user back to login page
